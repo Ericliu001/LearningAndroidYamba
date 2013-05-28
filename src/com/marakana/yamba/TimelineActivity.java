91f8ce89d55d00e372d9ef16d6078ec90879b617
cl kim <clkimcomputerorg@gmail.com>
2013-05-27 23:11:13 -0400

    implement LoaderManager to update ListView in TimelineActivity
    Specify min sdk version 11, instead of 4, to use LoaderManager class.
    Target build to run on 4.0.3 Ice Cream Sandwich (API 15).
    Change status EditText background to non-white for Holo's white font.
    Ref for implementation is
     http://developer.android.com/reference/android/app/LoaderManager.html

--- src/com/marakana/yamba/TimelineActivity.java
+++ src/com/marakana/yamba/TimelineActivity.java
@@ -1,21 +1,26 @@
 package com.marakana.yamba;
 
+import android.app.LoaderManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleCursorAdapter.ViewBinder;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.SimpleCursorAdapter.ViewBinder;
 
-public class TimelineActivity extends BaseActivity { //
+public class TimelineActivity extends BaseActivity
+                              implements LoaderManager.LoaderCallbacks<Cursor> { //
 	  static final String SEND_TIMELINE_NOTIFICATIONS = "com.marakana.yamba.SEND_TIMELINE_NOTIFICATIONS";
 	
 	  //DbHelper dbHelper;
@@ -31,6 +36,7 @@ public class TimelineActivity extends BaseActivity { //
 	  static final int[] TO = { R.id.textCreatedAt, R.id.textUser, R.id.textText }; // 
 	  TimelineReceiver receiver;
 	  IntentFilter filter;
+	  TimelineActivity timelineActivity = this;
 
 	  @Override
 	  protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +56,10 @@ public class TimelineActivity extends BaseActivity { //
 	    // Create the receiver
 	    receiver = new TimelineReceiver();
 	    filter = new IntentFilter( UpdaterIntentService.NEW_STATUS_INTENT );
-	    
+
+	    // initialize loader, //TODO need to be prepared to handle onLoadFinished() loader callback, per docs
+	    getLoaderManager().initLoader(0, null, this);
+
 	    // Connect to database
 	    //dbHelper = new DbHelper(this);  // 
 	    //db = dbHelper.getReadableDatabase();  // 
@@ -112,15 +121,17 @@ public class TimelineActivity extends BaseActivity { //
 	  // Responsible for fetching data and setting up the list and the adapter
 	  private void setupList() { // 
 	    // Get the data
-	    cursor = yamba.getStatusData().getStatusUpdates();
-	    startManagingCursor(cursor); // clk: deprecated, use CursorLoader instead
+//	    cursor = yamba.getStatusData().getStatusUpdates();
+//	    startManagingCursor(cursor); // clk: deprecated, use CursorLoader instead
 	    	// see android.app.LoaderManager reference
-	    	// and use ‘standard constructor’ signature for SimpleCursorAdapter()
+	    	// and use 'standard constructor' signature for SimpleCursorAdapter()
 	    // Setup Adapter
 	    // 	need sdk 11 to use new signature SimpleCursorAdapter(this, R.layout.row, cursor, FROM, TO, 0)
-	    adapter = new SimpleCursorAdapter(this, R.layout.row, cursor, FROM, TO); // deprecated in sdk 11
+//	    adapter = new SimpleCursorAdapter(this, R.layout.row, cursor, FROM, TO); // deprecated in sdk 11
+		adapter = new SimpleCursorAdapter(this, R.layout.row, null, FROM, TO, 0);
 	    adapter.setViewBinder(VIEW_BINDER); // 
 	    listTimeline.setAdapter(adapter);
+	    getLoaderManager().restartLoader(0, null, timelineActivity);
 	  }
 	  
 	  // View binder constant to inject business logic that converts a timestamp to
@@ -147,12 +158,43 @@ public class TimelineActivity extends BaseActivity { //
 	  class TimelineReceiver extends BroadcastReceiver { // 
 		  @Override
 		  public void onReceive(Context context, Intent intent) { // 
-			cursor = yamba.getStatusData().getStatusUpdates();
-		    adapter.changeCursor(cursor);
+			getLoaderManager().restartLoader(0, null, timelineActivity);
+//			cursor = yamba.getStatusData().getStatusUpdates();
+//		    adapter.changeCursor(cursor);
 		    //cursor.requery(); // deprecated
-			adapter.notifyDataSetChanged(); // 
+//			adapter.notifyDataSetChanged(); //
 		    Log.d("TimelineReceiver", "onReceived");
 		  }
 	  }
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		// We have only one Loader, so we don't care about the ID.
+		Uri baseUri = StatusProvider.CONTENT_URI;
+		// Now create and return a CursorLoader that will take care of
+
+		// creating a Cursor for the data being displayed.
+		return new CursorLoader(this, baseUri,
+				null, null, null,
+				StatusData.C_CREATED_AT + " DESC");
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		// Swap the new cursor in. (The framework will take care of closing the
+		// old cursor once we return.)
+		adapter.swapCursor(data);
+
+		// The list should now be shown.
+		adapter.notifyDataSetChanged();
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		// This is called when the last Cursor provided to onLoadFinished()
+		// above is about to be closed. We need to make sure we are no
+		// longer using it.
+		adapter.swapCursor(null);
+	}
 }
 
